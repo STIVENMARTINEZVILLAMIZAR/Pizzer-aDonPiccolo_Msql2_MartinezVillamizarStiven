95ef6204c9aafdfb6b2a418ebd5de4cb2d188c25
Alejandro <yasdmi048@gmail.com>
2025-12-10 08:12:09 -0500

    FEAT: :zap: CREATING BD

---
+++ BD/database.sql
@@ -0,0 +1,74 @@
+-- ############################################################ --
+-- BASE DE DATOS PIZZERIA DON PICCOLO                           --
+-- ############################################################ -- 
+
+DROP DATABASE IF EXISTS pizzeria_don_piccolo;
+CREATE DATABASE pizzeria_don_piccolo CHARACTER SET utf8mb4;
+USE pizzeria_don_piccolo;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ================================
+-- PERSONA
+-- ================================
+CREATE TABLE PERSONA (
+  idPERSONA INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(45) NOT NULL,
+  correo VARCHAR(45) NOT NULL,
+  telefono VARCHAR(45) NOT NULL,
+  direccion VARCHAR(100) NOT NULL
+);
+
+-- ================================
+-- CLIENTE (Hereda de PERSONA)
+-- ================================
+CREATE TABLE CLIENTE (
+  idCLIENTE INT PRIMARY KEY,
+  FOREIGN KEY (idCLIENTE) REFERENCES PERSONA(idPERSONA)
+);
+
+-- ================================
+-- PEDIDO
+-- ================================
+CREATE TABLE PEDIDO (
+  idPEDIDO INT AUTO_INCREMENT PRIMARY KEY,
+  idCLIENTE INT NOT NULL,
+  idREPARTIDOR INT NOT NULL,
+  fecha_pedido DATE NOT NULL,
+  metodo_pago VARCHAR(30) NOT NULL,
+  estado (ENUM: 'pendiente', 'preparacion', 'entregado', 'cancelado') NOT NULL,
+  total (DECIMAL(10,2)),
+  FOREIGN KEY (idCLIENTE) REFERENCES CLIENTE(idCLIENTE),
+  FOREIGN KEY (idREPARTIDOR) REFERENCES REPARTIDOR(idREPARTIDOR)
+);
+
+
+-- ================================
+-- PIZZAS
+-- ================================
+CREATE TABLE PIZZAS (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(45) NOT NULL,
+  tamaño VARCHAR(20) NOT NULL,
+  precio_base DECIMAL(10,2) NOT NULL,
+  tipo VARCHAR(45) NOT NULL,
+  estado VARCHAR(45) NOT NULL
+);
+
+-- ================================
+-- PEDIDO_PIZZA
+-- ================================
+CREATE TABLE PEDIDO_PIZZA (
+  idITEM INT AUTO_INCREMENT PRIMARY KEY,
+  idPEDIDO INT NOT NULL,
+  idPIZZA INT NOT NULL,
+  cantidad INT NOT NULL,
+  subtotal DECIMAL(10,2) NOT NULL,
+  FOREIGN KEY (idPEDIDO) REFERENCES PEDIDO(idPEDIDO),
+  FOREIGN KEY (idPIZZA) REFERENCES PIZZAS(id)
+);
+
+
+
+
+
